214111d076eac14ca36789cd59f016471c933071
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-03 18:01:03 +0700

    halfway video details component

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
-const VideoDetail = ()=>{
+import { Paper, Typography} from '@material-ui/core';
+
+const VideoDetail = ({video})=>{
     return(
-        <h1>
-            Video Detail
-        </h1>
+        <React.Fragment>
+            <Paper elevation={6} style={{height:'70%'}}>
+                <iframe frameBorder="0" height="100%" width="100%" title="Video Player" src="..."/>
+            </Paper>
+            <Paper elevation={6} style={{padding:'15px'}}>
+
+            </Paper>
+        </React.Fragment>
     )
 }
 
