6c10d28b86cfb7fac80f3a9324a3584402c15f94
dikiogres <85748069+dikiogres@users.noreply.github.com>
2022-05-03 18:30:55 +0700

    styling video list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -7,7 +7,11 @@ import VideoItem from './VideoItem';
 const VideoList = ({ videos })=>{
     const listOfVideos = videos.map((video, id)=><VideoItem key={id} video={video}/>)
 
-    return listOfVideos;
+    return(
+        <Grid container spacing={10}>
+            {listOfVideos}
+        </Grid>
+    )
 }
 
 export default VideoList;
